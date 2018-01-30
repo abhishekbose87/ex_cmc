154b42f366e09eb69cdd5a8bc33cc5c8487bc8f6
Abhishek Bose <abhishekbose87@gmail.com>
2018-01-30 12:09:22 +0530

    Add fetch_latest_price method

--- lib/ex_cmc.ex
+++ lib/ex_cmc.ex
@@ -3,4 +3,5 @@ defmodule ExCmc do
   alias ExCmc.API
 
   defdelegate fetch_latest_price(crypto_currency, fiat_currency), to: API
+  defdelegate fetch_latest_price(crypto_currency), to: API
 end
--- lib/ex_cmc/api.ex
+++ lib/ex_cmc/api.ex
@@ -1,9 +1,45 @@
 defmodule ExCmc.API do
 
+  def fetch_latest_price(crypto_currency) do
+
+    name = convert_code_to_name(crypto_currency)
+    url = "https://api.coinmarketcap.com/v1/ticker/#{name}/"
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts body
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+
+  end
+
   def fetch_latest_price(crypto_currency, fiat_currency) do
     IO.puts "Fetch latest price of #{crypto_currency} in #{fiat_currency}"
-    
 
+    name = convert_code_to_name(crypto_currency)
+    url = "https://api.coinmarketcap.com/v1/ticker/#{name}/?convert=#{fiat_currency}"
+
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts body
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+
+  end
+
+  defp convert_code_to_name(code) do
+    map = %{
+      "XRP" => "Ripple",
+      "BTC" => "Bitcoin",
+      "ETH" => "Ethereum",
+      "LTC" => "Litecoin",
+     }
+     map[code]
   end
 
 end
